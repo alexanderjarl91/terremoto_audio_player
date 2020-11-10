2233f38854ade72fefe80e463886c1d578f05269
alexanderjarl91 <69215066+alexanderjarl91@users.noreply.github.com>
2020-11-10 18:13:41 +0000

    Add files via upload

---
+++ songlist.js
@@ -0,0 +1,375 @@
+//SONG LIST
+const songList = document.querySelector(".songList");
+
+let allSongs = [];
+
+//
+function addNewSong(songSrc) {
+  //1. audio element created.
+
+  //2. Define the song as an object with the following keys and starting values
+  let song = {
+    cover: "https://i.ytimg.com/vi/oYadGMVZm0E/hqdefault.jpg",
+    artist: "Daddy Yankee",
+    title: "terremoto",
+    audio: songSrc,
+    liked: false,
+    songId: allSongs.length,
+    isPlaying: false,
+    currentSong: false,
+    currentTime: 0,
+  };
+  //3. Add it to the allSongs array
+  allSongs.push(song);
+  console.log("Song added to array");
+  saveToLocalStorage();
+  resetAllSongsDom();
+}
+
+//
+let addSongToDom = function (song) {
+  // 1. Create a DOM Div for the new song
+  let newSong = document.createElement("div");
+  newSong.classList.add("newSong");
+  songList.appendChild(newSong);
+  newSong.setAttribute("data-index", song.songId);
+
+  //2. Song cover added to the div
+  let newSongCover = document.createElement("img");
+  newSongCover.classList.add("newSongCover");
+  newSongCover.src = song.cover;
+  newSong.appendChild(newSongCover);
+
+  //3. Play button added to the div
+  let newSongPlayBtn = document.createElement("img");
+  newSongPlayBtn.src = "img/songlistPlay.svg";
+  newSongPlayBtn.classList.add("newSongPlayBtn");
+  newSong.appendChild(newSongPlayBtn);
+  newSongPlayBtn.addEventListener("click", playSong);
+  //3.1 Check if song is playing, and show play or pause accordingly
+  if (song.isPlaying) {
+    newSongPlayBtn.src = "img/songlistPause.svg";
+  } else {
+    newSongPlayBtn.src = "img/songlistPlay.svg";
+  }
+
+  //4. Song info div element created and added to the div
+  let newSongInfo = document.createElement("div");
+  newSongInfo.classList.add("newSongInfo");
+  newSong.appendChild(newSongInfo);
+
+  //4.1 Artist name element created in song info div
+  let newSongArtist = document.createElement("h3");
+  newSongArtist.innerHTML = song.artist;
+  newSongInfo.appendChild(newSongArtist);
+
+  //4.2 Song title element created in song info div
+  let newSongTitle = document.createElement("h4");
+  newSongTitle.innerHTML = song.title;
+  newSongInfo.appendChild(newSongTitle);
+
+  //5. Icons div created and added to new song div
+  let newSongIcons = document.createElement("div");
+  newSongIcons.classList.add("icons");
+  newSong.appendChild(newSongIcons);
+
+  //5.1.1 Heart icon created and added to new song div
+  let newSongHeart = document.createElement("img");
+  newSongHeart.classList.add("heart");
+  //5.1.2 Show red heart if liked = true, else show empty heart
+  newSongHeart.src = song.liked ? "img/Heart_full.svg" : "img/Heart.svg";
+  newSongIcons.appendChild(newSongHeart);
+  //5.1.3 Run likeSong function on click
+  newSongHeart.addEventListener("click", likeSong);
+
+  //5.2.1 Trash button created and added to new song div
+  let newSongTrash = document.createElement("img");
+  newSongTrash.classList.add("trash");
+  newSongTrash.src = "img/trash.svg";
+  newSongIcons.appendChild(newSongTrash);
+  //5.2.2 Run removeSong function on click
+  newSongTrash.addEventListener("click", removeSong);
+
+  //6. Append the audio element from addNewSong function to the newSong div
+  let newSongAudio = document.createElement("audio");
+  newSongAudio.classList.add("newAudio");
+  newSongAudio.src = song.audio;
+  newSongAudio.currentTime = song.currentTime;
+
+  newSong.appendChild(newSongAudio);
+  if (song.isPlaying) {
+    newSongAudio.play();
+  }
+
+  //7. Create a line div below all new songs to match style.
+  let newSongLine = document.createElement("div");
+  newSongLine.classList.add("line");
+  newSong.parentNode.appendChild(newSongLine);
+
+  console.log("Song added or changed in DOM");
+};
+
+// RESET ALL DOM - a function that removes all html from songlist and then puts it back in with newer data
+function resetAllSongsDom() {
+  //1. Clear the song list DOM element
+  document.querySelector(".songList").innerHTML = "";
+  //2. Add a song to the DOM for each song in the allSongs array
+  allSongs.forEach((song) => {
+    addSongToDom(song);
+    console.log("DOM Reset.");
+  });
+}
+
+// UPLOAD BUTTONS
+const realUploadBtn = document.querySelector("#real-upload");
+const addSongBtn = document.querySelector("#addSongBtn");
+
+//1. click the input button on click
+addSongBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  realUploadBtn.click();
+});
+//2. when input changes (gets a file), set the files source as songSrc
+realUploadBtn.addEventListener("change", function (e) {
+  e.preventDefault();
+  const fileInput = e.target;
+  let songSrc = fileInput.value;
+  console.log(songSrc);
+  // the song source has a stupid file path so we
+  songSrc = songSrc.toString();
+  songSrc = songSrc.substring(11);
+
+  console.log("Song uploaded.");
+  //3. Run the addNewSong with the songSrc (filepath of uploaded song) argument
+  addNewSong(songSrc);
+});
+
+// PLAY BUTTON
+function playSong(e) {
+  const clickedSongIndex = e.target.parentNode.getAttribute("data-index");
+  const clickedSong = allSongs[clickedSongIndex];
+  const currentlyPlayingSong = allSongs.find(function (song) {
+    return song.isPlaying;
+  });
+
+  const audioElement = document.querySelectorAll(".newAudio")[clickedSongIndex];
+
+  //1. set all songs to not the current song, then set clickedSong to current song.
+  allSongs.forEach((song) => {
+    song.currentSong = false;
+  });
+  clickedSong.currentSong = true;
+
+  //2. find song that is playing but is not current song, pause it, and set it to not the current song nor the one that isPlaying
+  ////
+  if (currentlyPlayingSong && clickedSong !== currentlyPlayingSong) {
+    audioElement.pause();
+    // MISSING: set songs duration to 00:00
+    currentlyPlayingSong.isPlaying = false;
+    currentlyPlayingSong.currentSong = false;
+  }
+
+  //3. check if the clicked song is playing, if it is then  set isplaying to false and pause, otherwise set it to true and play.
+  if (clickedSong.isPlaying) {
+    clickedSong.isPlaying = false;
+    clickedSong.currentTime = audioElement.currentTime;
+    audioElement.pause();
+  } else {
+    clickedSong.isPlaying = true;
+    audioElement.play();
+  }
+  //4. give the nowPlaying footer section the cover, artist name and song title of the clicked song
+  const songCover = document.querySelector("#currentCover");
+  songCover.src = clickedSong.cover;
+
+  const songArtist = document.querySelector("#artist");
+  songArtist.innerHTML = clickedSong.artist;
+
+  const songTitle = document.querySelector("#title");
+  songTitle.innerHTML = clickedSong.title;
+
+  //6. change the playbar play button image
+  updatePlaybarPlayBtnImg();
+  //5. reset the dom to match the true state of the array
+  resetAllSongsDom();
+}
+
+//SONG LIST ICONS
+// liked song function
+let likedSongs = [];
+function likeSong(e) {
+  //1. target the dom element of the selected objects id
+  const songId = e.target.parentNode.parentNode.getAttribute("data-index");
+  //2. set the selected objects liked value to whatevers its not
+  allSongs[songId].liked = !allSongs[songId].liked;
+  //3. filter all liked songs into an array called likedSongs
+  likedSongs = allSongs.filter((song) => song.liked == true);
+  console.log("Song added to likedSongs.");
+  //4. Save the array to localStorage
+  //console.log(allSongs[songId].currentTime);
+
+  let likedSongsAudio = document.querySelectorAll(".newAudio")[songId];
+  allSongs[songId].currentTime = likedSongsAudio.currentTime;
+  saveToLocalStorage();
+  resetAllSongsDom();
+}
+
+//remove song function
+function removeSong(e) {
+  //1. get this songs id
+  const songId = e.target.parentNode.parentNode.getAttribute("data-index");
+  //2. alert a confirmation window, remove the song object from the array if confirmed.
+  var remove = confirm(
+    `Are you sure you want to delete ${allSongs[songId].title}?`
+  );
+  if (remove) {
+    allSongs.splice(songId, 1);
+  }
+  console.log("Song removed.");
+  //3. reset the dom to match the true state of the array
+  resetAllSongsDom();
+  saveToLocalStorage();
+}
+
+//PLAYBAR
+
+////Play button
+const playBtn = document.querySelector(".play");
+const playBtnImg = document.querySelector("#playBtnImg");
+
+let updatePlaybarPlayBtnImg = function () {
+  for (song of allSongs) {
+    if (song.isPlaying) {
+      playBtnImg.src = "img/pause.svg";
+      break;
+    } else {
+      playBtnImg.src = "img/play.svg";
+    }
+  }
+};
+
+playBtn.addEventListener("click", function () {
+  //1. change the image depending if a song is playing or not
+  updatePlaybarPlayBtnImg();
+
+  //2. check if the current song is playing and pause if it is, else play.
+  allSongs.forEach((song) => {
+    if (song.currentSong) {
+      const songBtn = document.querySelectorAll(".newSongPlayBtn")[song.songId];
+      const dummyEvent = { target: songBtn };
+      playSong(dummyEvent);
+    }
+  });
+});
+
+////Forward button
+const forwardBtn = document.querySelector(".forward");
+forwardBtn.addEventListener("click", function () {
+  const currentlyPlayingSong = allSongs.find(function (song) {
+    return song.currentSong;
+  });
+  let nextSongId = currentlyPlayingSong.songId + 1;
+
+  allSongs[nextSongId].audio.currentTime = 0;
+  allSongs[nextSongId].currentSong = true;
+  allSongs[nextSongId].isPlaying = true;
+  currentlyPlayingSong.currentSong = false;
+  //currentlyPlayingSong.audio.pause();
+  currentlyPlayingSong.audio.currentTime = 0;
+  currentlyPlayingSong.isPlaying = false;
+  updatePlaybarPlayBtnImg();
+  resetAllSongsDom();
+});
+
+////Back Button
+const backBtn = document.querySelector(".back");
+backBtn.addEventListener("click", function () {
+  const currentlyPlayingSong = allSongs.find(function (song) {
+    return song.currentSong;
+  });
+  let lastSongId = currentlyPlayingSong.songId - 1;
+
+  allSongs[lastSongId].currentSong = true;
+  allSongs[lastSongId].isPlaying = true;
+  currentlyPlayingSong.currentSong = false;
+  //currentlyPlayingSong.audio.pause();
+  currentlyPlayingSong.audio.currentTime = 0;
+  currentlyPlayingSong.isPlaying = false;
+  updatePlaybarPlayBtnImg();
+  resetAllSongsDom();
+});
+
+// MENU
+const allSongsBtn = document.querySelector("#allSongsBtn");
+const likedSongsBtn = document.querySelector("#likedSongsBtn");
+
+////Liked songs
+likedSongsBtn.addEventListener("click", function () {
+  //1. clear the songLists DOM elements
+  document.querySelector(".songList").innerHTML = "";
+
+  //2. create DOM elements in the songList div for all song objects in allSongs array that have the key liked value = true
+  allSongs.forEach((song) => {
+    if (song.liked) {
+      addSongToDom(song);
+    }
+  });
+});
+
+////All songs
+//clear all songList Div elements, then create DOM elements for all allSongs objects in the songList div
+allSongsBtn.addEventListener("click", function () {
+  //1. clear the songLists DOM elements
+  document.querySelector(".songList").innerHTML = "";
+  //2. add DOM elements for all songs in the array
+  allSongs.forEach((song) => {
+    addSongToDom(song);
+  });
+});
+
+//SET ACTIVE MENU
+let btns = document.querySelectorAll(".menu-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+document.querySelector("#checkbox").addEventListener("click", function () {
+  document.documentElement.classList.toggle("dark-mode");
+});
+
+function clearAll() {
+  localStorage.clear();
+  allSongs = [];
+  resetAllSongsDom();
+}
+
+//Save to local storage
+function saveToLocalStorage() {
+  let allSongsString = JSON.stringify(allSongs);
+  localStorage.setItem("allSongs", allSongsString);
+}
+
+function loadLocalStorage() {
+  let allSongsDeStringed = JSON.parse(localStorage.getItem("allSongs"));
+
+  if (localStorage.length > 0)
+    for (song of allSongsDeStringed) {
+      allSongs.push(song);
+    }
+  resetAllSongsDom();
+}
+
+window.addEventListener("beforeunload", function () {
+  for (song of allSongs) {
+    song.isPlaying = false;
+    song.currentTime = 0;
+    saveToLocalStorage();
+    resetAllSongsDom();
+  }
+});
+
+loadLocalStorage();
